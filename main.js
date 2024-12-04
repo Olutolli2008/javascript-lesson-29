ae16e94616de6782c300c9a6ed37928d148accc9
Olutolli2008 <167331251+Olutolli2008@users.noreply.github.com>
2024-12-04 17:24:55 +0100

    ...

---
+++ main.js
@@ -0,0 +1,32 @@
+var button =  document.getElementById("button");
+
+var katrori = document.getElementById("katrori");
+
+button.onclick = function(){
+   alert("hello");
+}
+
+
+///arrays
+
+var students =["Fatmir","Rona","Erioni","Ylli"];
+
+console.log(students);
+
+console.log(students [3]);
+
+var number=[1,2,3,4,5];
+
+console.log(number [2]);
+
+students.push("Senad");
+
+console.log(students);
+
+students.pop(students);
+
+students.unshift(students);
+
+console.shift(students);
+
+
